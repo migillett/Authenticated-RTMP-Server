42a575c2b2e8e0309c5daf4e82f2dd4156c0119f
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-16 21:28:54 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Run this once to get everything setup
+
+apt update && sudo apt upgrade -y
+
+# definitely needed
+apt install nginx libnginx-mod-rtmp -y
+# needs testing?
+apt install build-essential libpcre3 libpcre3-dev libssl-dev
+
+# create directory to convert RTMP to HLS
+mkdir /tmp/hls
+chown -R www-data:www-data /tmp/hls
+
+# create directory for HTML website
+mkdir /var/www/html
+cp ./index.html /var/www/html
+chown -R www-data:www-data /var/www/html
+
+cp ./nginx.conf /etc/nginx
+service nginx restart
