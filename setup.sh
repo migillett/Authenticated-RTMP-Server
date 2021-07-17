a26fa85670637b225512a7cfacb043ab727f900a
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-16 21:45:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
-#! /bin/bash
+#!/bin/bash
 
 # Run this once to get everything setup
 # you must run this script as sudo or root
-# Works with Ubuntu 21
+# Tested with Ubuntu 21.04
 
 apt update && apt upgrade -y
 
 # definitely needed
 apt install nginx libnginx-mod-rtmp -y
-# needs testing?
-apt install build-essential libpcre3 libpcre3-dev libssl-dev
+# needs testing to see if truly needed
+apt install build-essential libpcre3 libpcre3-dev libssl-dev -y
 
 # create directory to convert RTMP to HLS
-mkdir /tmp/hls
+mkdir -p /tmp/hls
 chown -R www-data:www-data /tmp/hls
 
 # create directory for HTML website
-mkdir /var/www/html
+mkdir -p /var/www/html
 cp ./index.html /var/www/html
 chown -R www-data:www-data /var/www/html
 
