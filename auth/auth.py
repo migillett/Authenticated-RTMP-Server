d138a4cf1272725f95d07059ba1ca1c629a2f694
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-24 20:49:19 -0400

    move to testing

--- auth/auth.py
+++ auth/auth.py
@@ -7,7 +7,6 @@ from hash import hash_text
 app = Flask(__name__)
 
 
-<<<<<<< Updated upstream
 @app.route('/auth', methods=['GET'])
 def authorize_stream():
     hashed_key = hash_text(request.args['pwd'])
@@ -20,21 +19,3 @@ def authorize_stream():
 
 if __name__ == '__main__':
     app.run(host='localhost', debug=True, port=5000)
-=======
-@app.route('/auth')
-def auth():
-    try:
-        hashed_key = hash_text(request.args.get('pwd'))
-
-        if hashed_key in valid_keys:
-            return jsonify(message='Successful login'), 200
-
-        else:
-            return jsonify(message='Invalid stream key'), 403
-
-    except AttributeError:
-        return jsonify(message='Invalid URL'), 404
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
->>>>>>> Stashed changes
