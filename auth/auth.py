21a177a1a00616c8a7b7918e1fc413d47384b272
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-26 22:34:36 -0400

    networking test

--- auth/auth.py
+++ auth/auth.py
@@ -18,4 +18,4 @@ def authorize_stream():
         return jsonify(message='Invalid stream key'), 401
 
 if __name__ == '__main__':
-    app.run(host='172.100.0.3', debug=True, port=5000)
+    app.run(host='rtmp_auth', debug=True, port=5000)
