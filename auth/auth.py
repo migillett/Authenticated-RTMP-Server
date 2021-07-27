a7c700933d8619120a5aadaf66aa075f3b40a053
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-26 22:26:44 -0400

    Update auth.py

--- auth/auth.py
+++ auth/auth.py
@@ -18,4 +18,4 @@ def authorize_stream():
         return jsonify(message='Invalid stream key'), 401
 
 if __name__ == '__main__':
-    app.run(host='rtmp_auth', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
