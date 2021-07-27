e12282023e990780882f9e683240e50055cb0ea2
Michael Gillett <51103663+migillett@users.noreply.github.com>
2021-07-26 22:29:57 -0400

    minor revisions

--- auth/auth.py
+++ auth/auth.py
@@ -18,4 +18,4 @@ def authorize_stream():
         return jsonify(message='Invalid stream key'), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='172.100.0.3', debug=True, port=5000)
